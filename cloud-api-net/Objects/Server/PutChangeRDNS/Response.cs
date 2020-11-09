177689cba1d8746d8e83da06aa2a1b0d8fc9af31
Fabian Spottog <fabian@spottog.de>
2020-11-09 18:33:00 +0100

    Change Reverse DNS Pointer from a Server

---
+++ cloud-api-net/Objects/Server/PutChangeRDNS/Response.cs
@@ -0,0 +1,6 @@
+﻿namespace lkcode.hetznercloudapi.Objects.Server.PutChangeRDNS {
+    public class Response
+    {
+        public Objects.Server.Universal.ServerAction action { get; set; }
+    }
+}
