ba3638cd08c0b78a770b77f105e1188c7f764048
Lars Krämer <lars.kraemer@live.de>
2023-12-22 00:01:41 +0100

    HC-53: fixed accesstoken bug

--- source/Hetzner.Cloud/Services/HetznerCloudService.cs
+++ source/Hetzner.Cloud/Services/HetznerCloudService.cs
@@ -24,7 +24,7 @@ public class HetznerCloudService : IHetznerCloudService
             this._clientUserAgent = userAgent!;
 
         string? apiToken = hcloudConfig.GetSection("apitoken").Value;
-        if (string.IsNullOrEmpty(apiToken))
+        if (!string.IsNullOrEmpty(apiToken))
             this.LoadApiToken(apiToken!);
     }
 
@@ -36,10 +36,10 @@ public class HetznerCloudService : IHetznerCloudService
         if (this._httpClient is not null)
         {
             this._httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", this._apiToken);
-            // if (!string.IsNullOrEmpty(this._clientUserAgent))
-            // {
-            //     this._httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue(this._clientUserAgent));
-            // }
+            if (!string.IsNullOrEmpty(this._clientUserAgent))
+            {
+                this._httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue(this._clientUserAgent));
+            }
         }
     }
 
@@ -51,9 +51,9 @@ public class HetznerCloudService : IHetznerCloudService
         this._httpClient.BaseAddress = new Uri(API_SERVER);
 
         this._httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", this._apiToken);
-        // if (!string.IsNullOrEmpty(this._clientUserAgent))
-        // {
-        //     this._httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue(this._clientUserAgent));
-        // }
+        if (!string.IsNullOrEmpty(this._clientUserAgent))
+        {
+            this._httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue(this._clientUserAgent));
+        }
     }
 }
